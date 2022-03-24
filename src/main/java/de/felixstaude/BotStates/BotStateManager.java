46d95a55731d11234c6751374300141ea75a3a4b
felixstaude <felixstaude0112@gmail.com>
2022-03-24 19:25:56 +0100

    error commit

--- src/main/java/de/felixstaude/BotStates/BotStateManager.java
+++
@@ -1,5 +0,0 @@
-package de.felixstaude.BotStates;
-
-public class BotStateManager {
-
-}
--- src/main/java/de/felixstaude/main/BotMain.java
+++ src/main/java/de/felixstaude/main/BotMain.java
@@ -1,6 +1,5 @@
 package de.felixstaude.main;
 
-import de.felixstaude.BotStates.BotState;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.OnlineStatus;
@@ -10,37 +9,9 @@ import javax.security.auth.login.LoginException;
 
 public class BotMain {
 
-    public static JDA shardMan;
-    public static JDABuilder builder;
-
     public static void main(String[] args) throws LoginException {
-
-        builder = JDABuilder.createDefault("OTQ5MDA2ODk2NTUwMDAyNzQ4.YiEFiw.hG5tUL910s_J-_e627C_OcLj8EU");
-        shardMan = builder.build();
-
-    }
-
-    public static void setActivity(BotState state) {
-        switch (state){
-            case ONLINE:
-                builder.setStatus(OnlineStatus.ONLINE);
-                builder.setActivity(Activity.playing(BotState.ONLINE.label));
-
-                break;
-            case BOOTUP:
-                builder.setStatus(OnlineStatus.IDLE);
-                builder.setActivity(Activity.playing(BotState.BOOTUP.label));
-
-                break;
-            case SHUTDOWN:
-                builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
-                builder.setActivity(Activity.playing(BotState.SHUTDOWN.label));
-                break;
-            case MAINTENANCE:
-                builder.setStatus(OnlineStatus.DO_NOT_DISTURB);
-                builder.setActivity(Activity.playing(BotState.MAINTENANCE.label));
-                break;
-        }
+        JDABuilder builder = JDABuilder.createDefault("OTQ5MDA2ODk2NTUwMDAyNzQ4.YiEFiw.Zt0qeUwzpfryyMDnoCJ9AxGvwG8");
+        builder.build();
     }
 
 }
